213fb71d6cef4c8cbf353832aa79218476db2137
JuhahuJ <juha.lehtinen@helsinki.fi>
2024-05-15 17:16:29 +0300

    test deployment to render

--- part3/3.1/express-app/index.js
+++ part3/3.1/express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! I AM A CHANGE</h1>')
+  res.send('<h1>Hello World! I AM A CHANGE and I am another</h1>')
 })
 
 const PORT = 8080
